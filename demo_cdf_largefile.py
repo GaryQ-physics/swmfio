f01be5763d0f4a17b3ef04257a96bd8a32a7fdd5
Bob Weigel <rweigel@gmu.edu>
2022-01-04 09:42:45 -0500

    rename file

--- demo_cdf_largefile.py
+++ demo_cdf_largefile.py
@@ -4,18 +4,18 @@ from swmf_file_reader.batsrus_class import get_class_from_cdf
 
 urlbase = 'http://mag.gmu.edu/git-data/swmf_file_reader/demodata/'
 tmpdir = '/tmp/'
-filename = '3d__var_1_t00000000_n0002500.out.cdf'
-
+filename = '3d__var_2_e20190902-041000-000.out.cdf'
+    
 print("Downloading " + urlbase + filename)
 urlretrieve(urlbase + filename, tmpdir + filename)
 
 batsclass = get_class_from_cdf(tmpdir + filename)
 
 print( batsclass.data_arr.shape )
-# (1007616, 20)
+# (5896192, 19)
 
 print( batsclass.interpolate(np.array([1.,1.,1.]), 'rho') )
-# 28.0
+# 974.5159912109375
 
 print( batsclass.get_native_partial_derivatives(123456, 'rho') )
-# [ 1.8113604  -0.00352001  2.1863403 ]
+# [0.25239944 0.41480255 0.7658005 ]
