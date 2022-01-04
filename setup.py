24452dc32ef06746a36cbd105e76c46d8a16a1c5
Bob Weigel <rweigel@gmu.edu>
2022-01-04 13:20:22 -0500

    Add authors to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
-install_requires = ["numpy","scipy","numba"
-                    #cdflib, magnetovis
-                    ]
+install_requires = ["numpy","scipy","numba"]
+# install_requires.append("cdflib")
 
 setup(
     name='swmf_file_reader',
-    version='0.0.2',
+    version='0.0.3',
+    author='Gary Quaresima, Bob Weigel',
+    author_email='garyquaresima@gmail.com,rweigel@gmu.edu',
     packages=find_packages(),
-    description='reading swmf files using cdflib and scipy.io.FortranFiles',
+    description='Fast read and interpolate of native SWMF files',
     install_requires=install_requires
-     )
+)
